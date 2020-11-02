5f6d0f5038353dbb4d0f7057aa5a3a4bf213ae9a
Rajesh <rajeshsuramalla@gmail.com>
2020-11-02 19:30:37 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace AngularWithDotnetCoreElectronNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
