ecc94eaeb8e64cc6eb02d28aaa8432c03167aa58
Rajesh <rajeshsuramalla@gmail.com>
2022-12-14 09:27:13 +0000

    Resolved npm build issues

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,9 @@ namespace AngularWithDotnetCoreElectronNet
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName == "Development")
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -63,7 +63,7 @@ namespace AngularWithDotnetCoreElectronNet
 
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment())
+                if (env.EnvironmentName == "Development")
                 {
                     spa.UseAngularCliServer(npmScript: "start");
                 }
