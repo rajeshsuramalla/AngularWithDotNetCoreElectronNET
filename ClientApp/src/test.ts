68d768fcf3696beb726ebafae065a9e65e8d2c4c
Rajesh Suramalla <rajesh.suramalla@hallmarkhcs.com>
2023-01-27 00:22:08 +0530

    Angular upgraded to v15

--- ClientApp/src/test.ts
+++ ClientApp/src/test.ts
@@ -10,7 +10,5 @@ import {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  platformBrowserDynamicTesting()
 );
